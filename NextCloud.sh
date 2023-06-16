57734d2dfc13719319d608b41cc206d3137ba59e
jesp5555e <98485721+jesp5555e@users.noreply.github.com>
2023-06-16 11:00:09 +0200

    Update NextCloud.sh

--- NextCloud.sh
+++ NextCloud.sh
@@ -1,77 +1,48 @@
 #!/bin/bash
 
-#Installer Nginx-pakken
-yum update -y
-yum install -y nginx
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
 
-#Opret en ny Nginx-serverblok til Nextcloud
-tee /etc/nginx/sites-available/nextcloud > /dev/null <<EOT
-server {
-    listen 80;
-    server_name 192.168.224.4;
+mkdir /root/Nextcloud
+touch /root/Nextcloud/docker-compose.yaml
+#Opret en til Nextcloud
+tee /root/Nextcloud/docker-compose.yaml <<EOT
+version: '2'
 
-    # Tilføj eventuelle SSL-konfigurationer her
+volumes:
+  nextcloud:
+  db:
 
-    root /var/www/html/nextcloud;
+services:
+  db:
+    image: mariadb:10.6
+    restart: always
+    command: --transaction-isolation=READ-COMMITTED --log-bin=binlog --binlog-format=ROW
+    volumes:
+      - db:/var/lib/mysql
+    environment:
+      - MYSQL_ROOT_PASSWORD=Kode1234!
+      - MYSQL_PASSWORD=NextCloud
+      - MYSQL_DATABASE=nextcloud
+      - MYSQL_USER=nextcloud
 
-    location / {
-        try_files $uri $uri/ /index.php$request_uri;
-    }
-
-    location ~ ^/(?:.htaccess|data|config|dbstructure.xml|README) {
-        deny all;
-    }
-
-    location ~ ^/(?:build|tests|config|lib|3rdparty|templates|data)/ {
-        deny all;
-    }
-
-    location ~ ^/(?:.|autotest|occ|issue|indie|db|console) {
-        deny all;
-    }
-
-    location ~ ^/(?:index|remote|public|cron|core/ajax/update|status|ocs/v[12]|updater/.+|ocs-provider/.+).php(?:$|/) {
-        fastcgi_split_path_info ^(.+?.php)(/.*)$;
-        include fastcgi_params;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        fastcgi_param PATH_INFO $fastcgi_path_info;
-        fastcgi_param HTTPS on;
-        fastcgi_param modHeadersAvailable true;
-        fastcgi_param front_controller_active true;
-        fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
-        fastcgi_intercept_errors on;
-        fastcgi_request_buffering off;
-    }
-
-    location ~ ^/(?:updater|ocs-provider)(?:$|/) {
-        try_files $uri/ =404;
-        index index.php;
-    }
-
-    location ~ .(?:css|js|woff2?|svg|gif|map)$ {
-        try_files $uri /index.php$request_uri;
-        add_header Cache-Control "public, max-age=15778463";
-        add_header X-Content-Type-Options nosniff;
-        add_header X-Frame-Options "SAMEORIGIN";
-        add_header X-XSS-Protection "1; mode=block";
-        add_header X-Robots-Tag none;
-        add_header X-Download-Options noopen;
-        add_header X-Permitted-Cross-Domain-Policies none;
-        access_log off;
-    }
-
-    location ~ .(?:png|html|ttf|ico|jpg|jpeg)$ {
-        try_files $uri /index.php$request_uri;
-        access_log off;
-    }
-}
+  app:
+    image: nextcloud
+    restart: always
+    ports:
+      - 8080:80
+    links:
+      - db
+    volumes:
+      - nextcloud:/var/www/html
+    environment:
+      - MYSQL_PASSWORD=NextCloud
+      - MYSQL_DATABASE=nextcloud
+      - MYSQL_USER=nextcloud
+      - MYSQL_HOST=db
 EOT
 
-#Aktivér Nextcloud-serverblokken
-ln -s /etc/nginx/sites-available/nextcloud /etc/nginx/sites-enabled/
-
-#Fjern standardserverblokken
-rm /etc/nginx/sites-enabled/default
-
-#Genstart Nginx-tjenesten
-systemctl restart nginx
+cd /root/Nextcloud
+docker-compose up -d
