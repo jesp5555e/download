5940d4be8c3057975e2a783c97f173bf65d116c0
jesp5555e <98485721+jesp5555e@users.noreply.github.com>
2023-06-20 10:00:57 +0200

    Update FilServer.sh

--- FilServer.sh
+++ FilServer.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
 # Installer og konfigurer Samba-serveren
-yum update -y
-yum install samba -y
+dnf install samba samba-common samba-client 
 
 # Installer og konfigurer vsftpd FTP-serveren
 yum install vsftpd -y
 
 # Opret en delt mappe og konfigurer Samba
-mkdir /var/shared_folder
-chown nobody:nogroup /var/shared_folder
-chmod 770 /var/shared_folder
+sudo mkdir -p /srv/share/data
+chmod -R 755 /srv/share/data
+chown -R  nobody:nobody /srv/share/data
+chcon -t samba_share_t /srv/share/data
 
-echo "[shared_folder]" >> /etc/samba/smb.conf
-echo "path = /var/shared_folder" >> /etc/samba/smb.conf
+sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.bak
+touch /etc/samba/smb.conf
+
+echo "[global]" > /etc/samba/smb.conf
+echo "workgroup = WORKGROUP" >> /etc/samba/smb.conf
+echo "server string = Samba Server %v" >> /etc/samba/smb.conf
+echo "netbios name = rocky-8" >> /etc/samba/smb.conf
+echo "security = user" >> /etc/samba/smb.conf
+echo "map to guest = bad user" >> /etc/samba/smb.conf
+echo "dns proxy = no" >> /etc/samba/smb.conf
+echo "ntlm auth = true" >> /etc/samba/smb.conf
+
+echo "[Public]" >> /etc/samba/smb.conf
+echo "path =  /srv/share/data" >> /etc/samba/smb.conf
 echo "read only = no" >> /etc/samba/smb.conf
 echo "guest ok = yes" >> /etc/samba/smb.conf
+echo "browsable =yes" >> /etc/samba/smb.conf
+echo "writable = yes" >> /etc/samba/smb.conf
 
 # Konfigurer vsftpd
 echo "local_enable=YES" >> /etc/vsftpd.conf
@@ -25,7 +39,14 @@ echo "chroot_local_user=YES" >> /etc/vsftpd.conf
 echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf
 
 # Genstart Samba-tjenesten for at anvende konfigurationsændringer
-systemctl restart smbd
+systemctl start smb
+systemctl enable smb
+systemctl start nmb
+systemctl enable nmb
 
 # Genstart vsftpd-tjenesten for at anvende konfigurationsændringer
 systemctl restart vsftpd
+
+firewall-cmd --permanent --add-service=samba
+firewall-cmd --permanent --add-service=ftp
+firewall-cmd --reload
